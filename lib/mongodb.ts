f86a78f987b9ea62dc73395685d199950d9119a8
Pratith544 <pratithbhat@gmail.com>
2025-11-09 02:01:29 +0530

    Mongo connection changes

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -4,11 +4,15 @@ import { MongoClient } from "mongodb";
 let client: MongoClient | null = null;
 let db: any = null;
 
-export async function connectToMongoDB(uri: string) {
+export async function connectToMongoDB(uri: string, dbName?: string) {
   if (!client) {
     client = new MongoClient(uri);
     await client.connect();
-    db = client.db(); // default DB
+
+    // If dbName is provided, use it.
+    // Otherwise, use the database defined inside the URI.
+    db = dbName ? client.db(dbName) : client.db();
   }
+
   return { client, db };
 }
